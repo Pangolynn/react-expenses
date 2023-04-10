638ea5934ee3888fbd6bebecf4b60e8af984fee7
Samantha S <sami.berlynn@proton.me>
2023-04-09 22:27:52 -0400

    cleaned up reused css

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -3,7 +3,7 @@ import styles from "./Button.module.css";
 const Button = (props) => {
   return (
     <button
-      className={`${styles.buttonExpense} ${props.alt && styles.alternative} `}
+      className={`${styles.buttonExpense} ${props.alt && styles.alternative}`}
       type={props.type}
       onClick={props.onClick}
     >
